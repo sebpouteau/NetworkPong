cc325ef438f8375aa6347db2d4c3f9b07409da63
Ysabelle Emery <ysabelle.emery@etu.u-bordeaux.fr>
2015-11-27 16:55:28 +0100

    fichier menu

---
+++ pong/src/Menu/CreatePongDialogInfo.java
@@ -0,0 +1,26 @@
+package src.Menu;
+
+/**
+ * Created by ysaemery on 27/11/15.
+ */
+public class CreatePongDialogInfo {
+    private String numberPlayer;
+
+    public CreatePongDialogInfo(){}
+
+    public CreatePongDialogInfo(String numberPlayer){
+        this.numberPlayer = numberPlayer;
+
+    }
+
+    public String toString(){
+        String str;
+        if(this.numberPlayer != null) {
+            str = "Nombre de joueurs :";
+            str += numberPlayer;
+        }
+        else
+            str = "Aucune Information !";
+        return str;
+    }
+}
---
+++ pong/src/Menu/JoinPongDialogInfo.java
@@ -0,0 +1,29 @@
+package src.Menu;
+
+/**
+ * Created by ysaemery on 27/11/15.
+ */
+public class JoinPongDialogInfo  {
+    private String adress;
+    private String port;
+
+    public JoinPongDialogInfo(){}
+
+    public JoinPongDialogInfo(String adress, String port){
+        this.adress = adress;
+        this.port = port;
+    }
+
+    public String toString(){
+        String str;
+        if(this.adress != null && this.port != null) {
+            str = "Adresse : ";
+            str += adress;
+            str += " , port : ";
+            str += port;
+        }
+        else
+            str = "Aucune Information !";
+        return str;
+    }
+}
