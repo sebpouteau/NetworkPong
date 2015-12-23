d6f722315559bab521fa08b978c9a3cd782055c6
sebpouteau <sebastien.pouteau@etu.u-bordeaux1.fr>
2015-12-23 21:05:50 +0100

    modfication disign jeu, une couleur par raquette, score bien possitionné, modification image

--- pong/src/gui/Pong.java
+++ pong/src/gui/Pong.java
@@ -193,6 +193,22 @@ public class Pong extends JPanel {
                 null);
     }
 
+	private Color selectColor(PongItem racket){
+		switch(racket.getNumber()){
+			case 1:
+				return new Color(255, 229, 65);
+			case 2:
+				return new Color(47, 37, 255);
+			case 3:
+				return new Color(214, 30, 30) ;
+			case 4:
+				return  new Color(31, 197, 64);
+			default:
+				return Color.WHITE;
+
+		}
+	}
+
 	/**
 	 *
 	 */
@@ -206,18 +222,18 @@ public class Pong extends JPanel {
 				graphicContext = buffer.getGraphics();
 		}
 
-		/* Fill the area with blue */
-		graphicContext.setColor(Color.WHITE);
-		graphicContext.fillRect(0, 0, SIZE_WINDOW_X, SIZE_WINDOW_Y);
+
         graphicContext.setColor(backgroundColor);
         graphicContext.fillRect(0,0, SIZE_PONG_X, SIZE_PONG_Y);
 
+		int numberPlayer =0;
 		/* Draw items */
 		for (int i = 0; i < listItemSize(); i++) {
 			if(getItem(i) instanceof Racket){
-                graphicContext.setColor(new Color(197, 200, 60));
+				numberPlayer += getItem(i).getNumber();
+                graphicContext.setColor(selectColor(getItem(i)));
 				graphicContext.fillRect(getItem(i).getPositionX(), getItem(i).getPositionY(), getItem(i).getWidth(), getItem(i).getHeight());
-                graphicContext.setColor(backgroundColor);
+                //graphicContext.setColor(backgroundColor);
 			}
 			else if (getItem(i) instanceof Bonus) {
                 Bonus b =(Bonus) getItem(i);
@@ -229,16 +245,31 @@ public class Pong extends JPanel {
 				draw(getItem(i));
 			}
 		}
-        graphicContext.setColor(Color.BLACK);
+		graphicContext.setColor(Color.WHITE);
+		if (numberPlayer <=3)
+			graphicContext.drawLine(SIZE_PONG_X /2,0,SIZE_PONG_X/2,SIZE_PONG_Y);
+        else {
+			graphicContext.drawLine(0, 0, SIZE_PONG_X, SIZE_PONG_Y);
+			graphicContext.drawLine(SIZE_PONG_X , 0, 0, SIZE_PONG_Y);
+		}
 
-        for (int j = 0; j < listItemSize() ; j++) {
+
+
+
+		graphicContext.fillRect(0, SIZE_PONG_Y,SIZE_WINDOW_X,SIZE_WINDOW_Y);
+		graphicContext.setColor(Color.BLACK);
+		for (int j = 0; j < listItemSize() ; j++) {
             if(getItem(j) instanceof Racket){
                  int idPlayer = getItem(j).getNumber();
-                graphicContext.drawString("Joueur " + (idPlayer) + " : " + score[idPlayer - 1]+"   ", 0 + (idPlayer - 1)*110 , SIZE_PONG_Y + 20);
-				graphicContext.setFont(new Font("impact", Font.BOLD, 20));
+				graphicContext.setFont(new Font("impact", Font.ITALIC, 20));
+                graphicContext.drawString("Joueur " + (idPlayer) + " : " + score[idPlayer - 1]+"   ", 0 + (idPlayer - 1)*150 , SIZE_PONG_Y + 20);
+
 			}
         }
+
+
         this.repaint();
+		this.validate();
 
 	}
 }
